2248440fb5f423ead71fc47f17266784b72c23b8
matan <43654709+matanlaviv98@users.noreply.github.com>
2025-10-01 21:39:31 +0300

    added cors

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,5 @@
 import os
-
+from typing import List
 from pydantic_settings import BaseSettings
 
 
@@ -7,6 +7,7 @@ class Settings(BaseSettings):
     CLIENT_SECRETS_FILE: str = os.getenv("CLIENT_SECRETS_FILE")
     REDIRECT_URI: str = os.getenv("REDIRECT_URI")
     SCOPES: str = [scope for scope in os.getenv("SCOPES").split(",")]
+    CORS_HOSTS: List[str] = ["http://localhost:8080", "http://localhost:8000"]
 
 
 settings = Settings()
--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
-from fastapi.requests import Request
 import uvicorn
+from starlette.middleware.cors import CORSMiddleware
+from config.settings import settings
+from routes.connect.router import router as connect_router
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.CORS_HOSTS,
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],  # Allows all headers
+)
+
+app.include_router(connect_router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
