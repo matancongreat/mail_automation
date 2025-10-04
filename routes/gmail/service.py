bbb0cbceddc59c583b5fccd7e28a30b3fac14c7f
matan laviv <matanlaviv98@gmail.com>
2025-10-04 14:34:19 +0300

    refactoring

--- routes/gmail/service.py
+++ routes/gmail/service.py
@@ -3,6 +3,7 @@ from googleapiclient.discovery import build
 from google.oauth2.credentials import Credentials
 from config.settings import settings
 from typing import Dict, Any, List, Optional
+from models.user_credentials import UserCredentials
 
 
 class GmailService:
@@ -13,7 +14,7 @@ class GmailService:
 
     def __init__(self):
         # In-memory store for user credentials. Replace with DB in production.
-        self._user_credentials: Dict[str, Dict[str, Any]] = {}
+        self._user_credentials: Dict[str, UserCredentials] = {}
 
     def get_flow(self) -> Flow:
         """Create OAuth flow"""
@@ -44,14 +45,14 @@ class GmailService:
 
         # TODO: use real user id from session/JWT
         user_id = "user_123"
-        self._user_credentials[user_id] = {
-            'token': credentials.token,
-            'refresh_token': credentials.refresh_token,
-            'token_uri': credentials.token_uri,
-            'client_id': credentials.client_id,
-            'client_secret': credentials.client_secret,
-            'scopes': list(credentials.scopes) if credentials.scopes else []
-        }
+        self._user_credentials[user_id] = UserCredentials(
+            token=credentials.token,
+            refresh_token=credentials.refresh_token,
+            token_uri=credentials.token_uri,
+            client_id=credentials.client_id,
+            client_secret=credentials.client_secret,
+            scopes=list(credentials.scopes) if credentials.scopes else []
+        )
 
         return user_id
 
@@ -59,15 +60,10 @@ class GmailService:
         return user_id in self._user_credentials
 
     def _build_credentials(self, user_id: str) -> Credentials:
-        creds_data = self._user_credentials[user_id]
-        return Credentials(
-            token=creds_data['token'],
-            refresh_token=creds_data['refresh_token'],
-            token_uri=creds_data['token_uri'],
-            client_id=creds_data['client_id'],
-            client_secret=creds_data['client_secret'],
-            scopes=creds_data['scopes']
-        )
+        user_creds = self._user_credentials[user_id]
+        # Use model dict unpacking to avoid repeating field names
+        creds_data = user_creds.dict(exclude_none=True)
+        return Credentials(**creds_data)
 
     def build_service(self, user_id: str):
         creds = self._build_credentials(user_id)
